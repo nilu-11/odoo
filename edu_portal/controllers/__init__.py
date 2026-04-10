a6ded15f9eb0c9f3fbb1b4aebeb456ad4a96cf46
Abhinav Bajimaya <abhinavbajimaya1@gmail.com>
2026-04-10 12:33:45 +0545

    feat(edu_portal): scaffold module with manifest and init files

---
+++ edu_portal/controllers/__init__.py
@@ -0,0 +1,5 @@
+from . import helpers
+from . import main
+from . import teacher
+from . import student
+from . import parent
---
+++ edu_portal/models/__init__.py
@@ -0,0 +1,4 @@
+from . import res_users
+from . import edu_student
+from . import edu_guardian
+from . import hr_employee
